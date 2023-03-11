09ec8a657bb210655a3f37baa20eb4107c9ac83f
Richard Kwasnicki <richard.kwasnicki@gmx.de>
2023-03-11 19:13:50 +0100

    improved styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,9 +8,13 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroNotesComponent } from './hero-notes/hero-notes.component';
 import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatListModule} from '@angular/material/list'
+import {MatButtonModule} from '@angular/material/button'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDividerModule} from '@angular/material/divider'
 
 @NgModule({
   declarations: [
@@ -26,9 +30,15 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
     MatCardModule,
     MatInputModule,
     MatToolbarModule,
-    BrowserAnimationsModule
+    MatListModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDividerModule
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

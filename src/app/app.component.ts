8c850d21b692b4cb0388fcae2e38b5e3ef654cb1
rkwasnicki <rkwasnicki@avantgarde-labs.de>
2023-03-21 19:12:07 +0100

    use nicer smaller iconify icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,22 @@ import swordCross from '@iconify/icons-mdi/sword-cross';
 import autoFix from '@iconify/icons-mdi/auto-fix';
 import handsPray from '@iconify/icons-mdi/hands-pray';
 import bagSuitcase from '@iconify/icons-mdi/bag-suitcase';
+import contentSave from '@iconify/icons-mdi/content-save';
+import viewDashboard from '@iconify/icons-mdi/view-dashboard';
+import cardText from '@iconify/icons-mdi/card-text';
+import pickaxe from '@iconify/icons-mdi/pickaxe';
+import calendarText from '@iconify/icons-mdi/calendar-text';
 import {IconService} from "@visurel/iconify-angular";
 
 export const appIcons = {
   'sword-cross': swordCross,
   'magic': autoFix,
+  'pickaxe': pickaxe,
+  'calendar-text': calendarText,
+  'text': cardText,
+  'dashboard': viewDashboard,
   'liturgic': handsPray,
+  'save': contentSave,
   'inventory': bagSuitcase
 }
 

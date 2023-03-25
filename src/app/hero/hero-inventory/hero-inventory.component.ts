017063b5becf2c9cd8e032c016deb6afda02e63a
rkwasnicki <rkwasnicki@avantgarde-labs.de>
2023-03-25 16:39:41 +0100

    add inventory; restyle fight a bit

--- src/app/hero/hero-inventory/hero-inventory.component.ts
+++ src/app/hero/hero-inventory/hero-inventory.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HeroService} from "../../shared/services/hero.service";
+import {AbstractHeroComponent} from "../abstract-hero-component";
 
 @Component({
   selector: 'app-hero-inventory',
   templateUrl: './hero-inventory.component.html',
   styleUrls: ['./hero-inventory.component.scss']
 })
-export class HeroInventoryComponent {
+export class HeroInventoryComponent extends AbstractHeroComponent {
+  itemAttributeColumns: string[] = ["name", "amount", "weight", "price"]
+
+  constructor(heroService: HeroService, route: ActivatedRoute) {
+    super(route, heroService);
+  }
+
+  addItem() {
+    if (this.hero) {
+      this.hero.items.push({
+        name: "",
+        amount: 0,
+        weight: 0,
+        price: 0
+      })
+    }
+  }
+
+  deleteItem(i: number) {
+    if (this.hero) {
+      this.hero.items.splice(i, 1)
+    }
+  }
 
 }

ae742b0acceb19bdf6f51a6df8c5722be6dcd912
rkwasnicki <rkwasnicki@avantgarde-labs.de>
2023-03-25 20:38:20 +0100

    add tooltip for magic and liturgic

--- src/app/hero/abstract-hero-component.ts
+++ src/app/hero/abstract-hero-component.ts
@@ -67,4 +67,28 @@ export abstract class AbstractHeroComponent implements OnInit, OnDestroy {
   readOnly(): boolean {
     return this.hero?.creator_id !== this.authService.userData?.uid
   }
+
+  resolveProbe(probe: string): string {
+    return probe.split("/").map(p => {
+      if (p === "MU") {
+        return this.hero?.hero_stats?.mu.toString() ?? ""
+      } else if (p === "KL") {
+        return this.hero?.hero_stats?.kl.toString() ?? ""
+      } else if (p === "IN") {
+        return this.hero?.hero_stats?.in.toString() ?? ""
+      } else if (p === "CH") {
+        return this.hero?.hero_stats?.ch.toString() ?? ""
+      } else if (p === "FF") {
+        return this.hero?.hero_stats?.ff.toString() ?? ""
+      } else if (p === "GE") {
+        return this.hero?.hero_stats?.ge.toString() ?? ""
+      } else if (p === "KO") {
+        return this.hero?.hero_stats?.ko.toString() ?? ""
+      } else if (p === "KK") {
+        return this.hero?.hero_stats?.kk.toString() ?? ""
+      } else {
+        return ""
+      }
+    }).join("/")
+  }
 }
--- src/app/hero/hero-detail/hero-detail.component.ts
+++ src/app/hero/hero-detail/hero-detail.component.ts
@@ -21,28 +21,4 @@ export class HeroDetailComponent extends AbstractHeroComponent {
 
   attributeColumns: string[] = ['name', 'probe', 'fw'];
 
-  resolveProbe(probe: string): string {
-    return probe.split("/").map(p => {
-      if (p === "MU") {
-        return this.hero?.hero_stats?.mu.toString() ?? ""
-      } else if (p === "KL") {
-        return this.hero?.hero_stats?.kl.toString() ?? ""
-      } else if (p === "IN") {
-        return this.hero?.hero_stats?.in.toString() ?? ""
-      } else if (p === "CH") {
-        return this.hero?.hero_stats?.ch.toString() ?? ""
-      } else if (p === "FF") {
-        return this.hero?.hero_stats?.ff.toString() ?? ""
-      } else if (p === "GE") {
-        return this.hero?.hero_stats?.ge.toString() ?? ""
-      } else if (p === "KO") {
-        return this.hero?.hero_stats?.ko.toString() ?? ""
-      } else if (p === "KK") {
-        return this.hero?.hero_stats?.kk.toString() ?? ""
-      } else {
-        return ""
-      }
-    }).join("/")
-  }
-
 }
